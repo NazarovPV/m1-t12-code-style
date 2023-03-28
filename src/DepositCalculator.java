aab02741eb31011d0a5c0637ade05660ec9a34e5
NazarovPV <nazar969@yandex.ru>
2023-03-28 09:41:40 +0300

    Редактирование
    Приведение кода к стандартам code style

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,32 +1,37 @@
-import java.net.URI;
 import java.util.Scanner;
-import java.io.IOException;
 
-public class calculate_deposit {
+public class DepositCalculator {
 
-    double calculateComplexPercentFunction(double a, double y, int d ) {
-       double pay = a * Math.pow((1 + y / 12), 12 * d);
-       return rnd(pay, 2);
-    }
-    double calculateSimplePercentFunction(double doubleAmount, double doubleYearRate, int depositPeriod) {
-         return rnd(doubleAmount + doubleAmount * doubleYearRate * depositPeriod, 2);
-    }
-    double rnd(double value, int places) {
-       double scaLe = Math.pow(10, places);
-       return Math.round(value * scaLe) / scaLe;
+    double calculateComplexPercentFunction(double amount, double yearRate, int depositPeriod ) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return roundNumber(pay, 2);
     }
 
-    void doImportantJob() {
+    double calculateSimplePercentFunction(double amount, double yearRate, int depositPeriod) {
+        return roundNumber(amount + amount * yearRate * depositPeriod, 2);
+    }
+
+    double roundNumber(double value, int places) {
+        double scaLe = Math.pow(10, places);
+        return Math.round(value * scaLe) / scaLe;
+    }
+
+    void readInput() {
         int period;
         int action;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
+
         int amount = scanner.nextInt();
+
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
+
         double outDoubleVar = 0;
+
         if (action == 1) {
             outDoubleVar = calculateSimplePercentFunction(amount, 0.06, period);
         } else if (action == 2){
@@ -34,8 +39,9 @@ public class calculate_deposit {
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
     }
+
     public static void main(String[] args) {
-        new calculate_deposit().doImportantJob();
+        new DepositCalculator().readInput();
     }
 
 
